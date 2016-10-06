aaa90efc1d5abb5b23303247582352a3c950a1a0
giuliogallerini <giulio.gallerini.work.com>
2016-10-06 14:32:37 +0200

    add show specific of dinosaur and new dinosaur

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "dinosaurs#index"
   get "dinosaurs" => "dinosaurs#index"
+  get "dinosaurs/new" => "dinosaurs#new", as: :new_dinosaur
+  post "dinosaurs" => "dinosaurs#create"
   get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
 end
